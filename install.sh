6c111c7f1576114531854cb1cee957e7d0d68647
Shikigami <mazenkassem101@gmail.com>
2026-05-25 23:21:55 +0300

    adding dwmblocks

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+var='sudo make clean install'
+
 echo "Building suckless software..."
-cd ~/Dotfiles/suckless/dwm && sudo make clean install
-cd ../st && sudo make clean install
-cd ../dmenu && sudo make clean install
+cd ~/Dotfiles/suckless/dwm && $var
+cd ../slock && $var
+cd ../dmenu && $var
+cd ../dwmblocks && $var
 
 echo "#############################"
 echo "########    Done!    ########"
