4c132eeefc2f6c078afe4a8ced2a986ddd5eaa92
Shikigami <mazenkassem101@gmail.com>
2026-05-25 03:02:35 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+---
+
+[image](Desktop/desktop1.png)
+
+---
+
+[image](Desktop/desktop2.png)
+
+---
+
+[image](Desktop/desktop3.png)
+
+---
+
+[image](Desktop/desktop4.png)
+
+---
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    alias f='fastfetch'
+    alias c='code'
+    alias gc='git commit'
+    alias p='poweroff'
+    alias r='sudo reboot now'
+    alias s='sudo pacman -S'
+    alias up='sudo pacman -Syu'
+    alias lang='setxkbmap -layout us,ara -option grp:alt_shift_toggle'
+    alias tt='tt -showwpm -n 25'
+    alias cdwm='code ~/suckless/'
+    alias mdwm='cd ~/suckless/dwm; sudo make clean install; cd ~'
+    alias mst='cd ~/suckless/st; sudo make clean install; cd ~'
+    alias mdmenu='cd ~/suckless/dmenu; sudo make clean install; cd ~'
+end
+
+starship init fish | source
