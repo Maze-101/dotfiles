b53b8b643227bc04c55f346f9320d5fa6e17e68c
Shikigami <mazenkassem101@gmail.com>
2026-06-01 22:23:21 +0300

    adding alias for gemini cli

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     alias ff='fastfetch'
     alias cdo='code ~/dotfiles/'
     alias mdwm='cd ~; ./dotfiles/scripts/make.sh; cd -'
+    alias gem='npx @google/gemini-cli'
     ########### git #############
     alias gc='git commit -m'
     alias gp='git push -u origin main'
