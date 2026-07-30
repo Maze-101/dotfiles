21657dfb4a692b2f273756fb4b9ff77b9d48d539
Mazen Kassem <mazen.kassem.dev@gmail.com>
2026-07-30 06:03:55 +0300

    full bruh

--- nvim/.config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
-
-vim.diagnostic.config({
-  virtual_lines = false, -- Disables multi-line tree display below the code
-  virtual_text = true,   -- Enables inline diagnostics on the same line
-})
