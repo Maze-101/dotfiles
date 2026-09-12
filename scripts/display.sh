63dd617cb62f05a7cad1478b29203402feb949ea
Mazen Kassem <mazen.kassem.dev@gmail.com>
2026-09-12 11:17:45 +0300

    changing distros...

--- scripts/display.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-systemctl --user import-environment DISPLAY XAUTHORITY XDG_CURRENT_DESKTOP
-systemctl --user restart xdg-desktop-portal
-systemctl --user restart xdg-desktop-portal-gtk
